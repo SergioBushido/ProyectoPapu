4a353e7efac776639ada332c538858a1d5b70892
sergi <sergio.ume@hotmail.com>
2024-02-09 14:04:32 +0000

    Primer commit

---
+++ src/main/java/es/comicon/comic/controllers/ProductController.java
@@ -0,0 +1,19 @@
+package es.pixar.comicom.controllers;
+
+import es.pixar.comicom.models.Product;
+import es.pixar.comicom.repositories.ProductRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class ProductController {
+
+    private ProductRepository productRepository;//metemos como atributo la clase que ademas tiene la interface
+
+@GetMapping("product/{id}")
+public Product getProduct(@PathVariable int id) throws Exception {
+    return productRepository.findById(id).orElseThrow(() -> new Exception("Producto not found with id: " + id));
+}
+
+}
