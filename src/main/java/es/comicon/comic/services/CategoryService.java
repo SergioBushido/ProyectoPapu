2c097ab483dcdcedceb599753c2fae3d543cee18
sergi <sergio.ume@hotmail.com>
2024-03-05 08:53:57 +0000

    remove unnused imports

--- src/main/java/es/comicon/comic/services/CategoryService.java
+++ src/main/java/es/comicon/comic/services/CategoryService.java
@@ -7,14 +7,9 @@ import lombok.AllArgsConstructor;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 
 @Service //esta anotacion hace que sea un servicio o un bean que springboot lo escanea automaticamente
 @AllArgsConstructor//notacion lombok
